4d7a664c0401b100df1d8333431c9252d9094930
Nicolas Casajus <ahasverus@users.noreply.github.com>
2024-04-25 08:52:25 +0200

    build: update pipeline

--- make.R
+++ make.R
@@ -9,6 +9,7 @@
 #' @date 2024/03/06
 
 
+start <- Sys.time()
 
 ## Install Dependencies (listed in DESCRIPTION) ----
 
@@ -20,12 +21,45 @@ devtools::install_deps(upgrade = "never")
 devtools::load_all(here::here())
 
 
-## Global Variables ----
-
-# You can list global variables here (or in a separate R script)
-
-
 ## Run Project ----
 
-# List all R scripts in a sequential order and using the following form:
-# source(here::here("analyses", "script_X.R"))
+### Create study area grid ----
+
+world_grid <- create_grid()
+
+
+### Import Mammals spatial layers ----
+
+mammals <- mammals_read_data()
+
+
+### Update taxonomy ----
+
+mammals <- mammals_rename_subpop(mammals)
+mammals <- mammals_rename_species(mammals)
+
+
+### Extract species info ----
+
+taxonomy <- mammals_get_species_info(mammals)
+taxonomy <- mammals_match_taxonomies(taxonomy)
+
+write.csv(taxonomy, here::here("outputs", "mammals_iucn_taxonomy.csv"), 
+          row.names = FALSE)
+
+
+### Compute range sizes (~ 30mins) ----
+
+range_sizes <- mammals_range_sizes(mammals, world_grid, n_cores = 20)
+
+write.csv(range_sizes, here::here("outputs", "mammals_iucn_range_sizes.csv"),
+          row.names = FALSE)
+
+
+### Intersect polygons with raster (~ 30mins) ----
+
+occs_by_cells <- mammals_occ_on_cells(mammals, world_grid, n_cores = 20)
+
+qs::qsave(occs_by_cells, here::here("outputs", "mammals_iucn_occs_by_cells.qs"))
+
+print(Sys.time() - start)
